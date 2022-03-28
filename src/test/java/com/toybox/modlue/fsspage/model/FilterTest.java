9dbe636ae170644661376ce799d3d792f929bcc7
geonhee.kim <geonhee.kim@atto-research.com>
2022-03-29 02:01:49 +0900

    init

---
+++ src/test/java/com/toybox/modlue/fsspage/model/FilterTest.java
@@ -0,0 +1,13 @@
+package com.toybox.modlue.fsspage.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterTest {
+
+    @Test
+    void test(){
+        System.out.println("test");
+    }
+}
